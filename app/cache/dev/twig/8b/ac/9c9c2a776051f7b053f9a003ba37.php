048e24dac325dcb3d0a2a92322121ab49578491b
benoit brayer <brayer.benoit@gmail.com>
2012-11-10 17:51:01 +0100

    little design changes

--- app/cache/dev/twig/8b/ac/9c9c2a776051f7b053f9a003ba37.php
+++ app/cache/dev/twig/8b/ac/9c9c2a776051f7b053f9a003ba37.php
@@ -24,7 +24,7 @@ class __TwigTemplate_8bac9c9c2a776051f7b053f9a003ba37 extends Twig_Template
     protected function doDisplay(array $context, array $blocks = array())
     {
         // line 3
-        $context["__internal_32db8511abbeb05c413a6525752f93d78d1c7270"] = $this;
+        $context["__internal_9e64778079f56b327e7a9d2e9c50679b087438b1"] = $this;
         $this->parent->display($context, array_merge($this->blocks, $blocks));
     }
 
@@ -92,7 +92,7 @@ class __TwigTemplate_8bac9c9c2a776051f7b053f9a003ba37 extends Twig_Template
             echo "</code></td>
                 <td><code>";
             // line 36
-            echo $context["__internal_32db8511abbeb05c413a6525752f93d78d1c7270"]->getdisplay_listener($this->getContext($context, "listener"));
+            echo $context["__internal_9e64778079f56b327e7a9d2e9c50679b087438b1"]->getdisplay_listener($this->getContext($context, "listener"));
             echo "</code></td>
             </tr>
         ";
@@ -135,7 +135,7 @@ class __TwigTemplate_8bac9c9c2a776051f7b053f9a003ba37 extends Twig_Template
                 echo "</code></td>
                     <td><code>";
                 // line 55
-                echo $context["__internal_32db8511abbeb05c413a6525752f93d78d1c7270"]->getdisplay_listener($this->getAttribute($this->getContext($context, "listeners"), $this->getContext($context, "listener"), array(), "array"));
+                echo $context["__internal_9e64778079f56b327e7a9d2e9c50679b087438b1"]->getdisplay_listener($this->getAttribute($this->getContext($context, "listeners"), $this->getContext($context, "listener"), array(), "array"));
                 echo "</code></td>
                 </tr>
             ";
